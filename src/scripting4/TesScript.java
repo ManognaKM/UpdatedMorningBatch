4a1e7d3d008c8bc95ce4c6271e94f482b05c5199
manu <manognakm360@gmail.com>
2026-07-15 11:28:51 +0530

    Added new line

--- src/scripting4/TesScript.java
+++ src/scripting4/TesScript.java
@@ -25,6 +25,9 @@ public class TesScript {
             AppDependentClass.navigateURL(oBrowser, objData.get("appURL"));
             AppDependentClass.loginToActiTime(oBrowser, objData.get("userName"), objData.get("password"));
             AppDependentClass.logoutFromActiTime(oBrowser);
+
+
+            System.out.println("JUSt a Dummy line");
         }catch(Exception e){
             System.out.println("Exception in 'TS_LoginAndLogout()' testScript: "+ e);
         }finally{
